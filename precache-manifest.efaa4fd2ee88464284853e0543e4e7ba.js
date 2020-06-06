dff7ba0a781cd61fcfbe19897c405f8967ea6487
Gosols <teemu.a.kosonen@hotmail.com>
2020-06-06 16:49:40 +0300

    Updates

---
+++ precache-manifest.efaa4fd2ee88464284853e0543e4e7ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0022cc120467ea7ec6a0d9f5c0120ee9",
+    "url": "/final-task/index.html"
+  },
+  {
+    "revision": "c27a204441b6e40fcf45",
+    "url": "/final-task/static/css/2.b3b7618b.chunk.css"
+  },
+  {
+    "revision": "85b2f17b041ff9c4dd83",
+    "url": "/final-task/static/css/main.7ee408aa.chunk.css"
+  },
+  {
+    "revision": "c27a204441b6e40fcf45",
+    "url": "/final-task/static/js/2.f1bdb64e.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/final-task/static/js/2.f1bdb64e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b2f17b041ff9c4dd83",
+    "url": "/final-task/static/js/main.e6d2acd5.chunk.js"
+  },
+  {
+    "revision": "e99edc1b653c98ac4b78",
+    "url": "/final-task/static/js/runtime-main.3d2ae853.js"
+  }
+]);
